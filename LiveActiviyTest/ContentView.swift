0586ab2a16a052308731b37e44727016634b0582
Ray Kuo <wei_kuo@addcn.com>
2024-06-27 14:03:59 +0800

    Initial Commit

---
+++ LiveActiviyTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LiveActiviyTest
+//
+//  Created by 郭瑋 on 2024/6/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
